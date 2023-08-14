2cfc898dfa2ce9feb1f74a25d0c020c676ea0ce8
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-14 08:15:02 -0500

    test: :white_check_mark: Pass test get collection from mongodb
    Pass test get collection from mongodb to node

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import appcampus from "./routers/prueba.js";
+import prueba from "./routers/prueba.js";
 dotenv.config();
+
 const appExpress = express();
-appExpress.use(express.json());
-appExpress.use("/prueba", appcampus);
+appExpress.use("/prueba", prueba);
 
 const config = JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>{
-    console.log(`http://${config.hostname}:${config.port}`);
+   console.log(`http://${config.hostname}:${config.port}`);
 });
--- routers/prueba.js
+++ routers/prueba.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { conx } from "../db_mg/atlas.js";
 
-const appcampus = express();
-appcampus.use(express.json());
+const prueba = express();
+prueba.use(express.json());
 
-appcampus.get("/", async (req, res) => {
-    if(!req.rateLimit) return;
+prueba.get("/", async (req, res) => {
     let db = await conx();
     let colleccion = db.collection("bodegas");
     let result = await colleccion.find({}).toArray();
     res.send(result);
 });
 
-export default appcampus;
+
+export default prueba;
