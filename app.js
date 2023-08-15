60f453e2688639da1d02c5138b2e9696f885b946
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-15 09:23:51 -0500

    feat: :sparkles: Add Json Web Tokens validation of Endpoints
    Add Json Web Tokens validation of Endpoints

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import appHis from "./routers/historiales.js";
+import { appToken, appVerify } from "./limit/token.js";
 dotenv.config();
 
 const appExpress = express();
-appExpress.use("/historiales", appHis);
-
+appExpress.use("/historiales", appVerify, appHis);
+appExpress.use("/token", appToken);
 const config = JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>{
    console.log(`http://${config.hostname}:${config.port}`);
---
+++ limit/token.js
@@ -0,0 +1,58 @@
+import 'reflect-metadata';
+import {plainToClass, classToPlain } from 'class-transformer';
+import dotenv from 'dotenv';
+import {Router} from 'express';
+import { SignJWT, jwtVerify } from 'jose';
+import {Historial} from "../dtocontroller/historiales.js";
+
+dotenv.config();
+const appToken = Router();
+const appVerify = Router();
+
+
+const DTO = (p1) =>{
+    const match = {
+        'historiales': Historial
+    };  
+    const instan = match[p1];
+    if(!instan) throw {status:404, error:"Not Found", message:"Invalid Token :("}
+    return {atributos: plainToClass(instan, {},{ ignoreDecorators:true }), class: instan}  
+};
+
+appToken.use("/:collection", async(req,res)=>{
+    try {
+        let inst = DTO(req.params.collection).atributos;
+        const encoder = new TextEncoder();
+        const jwtconstructor = new SignJWT(Object.assign({}, classToPlain(inst)));
+        const jwt = await jwtconstructor
+        .setProtectedHeader({alg:"HS256", typ: "JWT"})
+        .setIssuedAt()
+        .setExpirationTime("30m")
+        .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+        req.data = jwt;
+        res.status(201).send({status: 201, message:"Created", result: jwt});
+    } catch (e) {
+        res.status(404).send({status: 404, error:"Not Found", message: e.error});
+    }
+})
+
+appVerify.use("/", async(req,res,next)=>{
+    const {authorization} = req.headers;
+    if (!authorization) return res.status(400).send({status: 400, token: "Missing Authorization Token :("});
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            authorization,
+            encoder.encode(process.env.JWT_PRIVATE_KEY)
+        );
+        req.data = jwtData;
+        next();
+    } catch (error) {
+        res.status(498).send({status: 498, token: "The Token Has Expired :("});
+    }
+})
+
+export {
+    appToken,
+    appVerify
+};
