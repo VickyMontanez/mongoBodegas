0693905de61ae099d94d50203361e5cbb0639ac4
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-17 16:27:52 -0500

    feat: :sparkles: Add Third Query Completed
    Add Third Query: list all products in order descending by the "Total" field Completed

--- queries/productos.js
+++ queries/productos.js
@@ -16,8 +16,43 @@ export async function getTotal(req, res){
     try {
         let db = await conx();
         let colleccion = db.collection("productos");
-        let results = await colleccion.find({}).sort({ nombre: 1 }).toArray();
-        results.length > 0 ? res.send(results).status(200) : res.status(404).send({ status: 404, message: "Found But Without Contain :(" })
+        let query = [
+            {
+                $lookup: {
+                    from: "inventarios",
+                    localField: "id",
+                    foreignField: "id_producto",
+                    as: "Total"
+                }
+            },
+            {
+                $match: {
+                    "Total": { $ne: [] }
+                }
+            },
+            {
+                $unwind: "$Total"
+            },
+            {
+                $group: {
+                    _id: "$id",
+                    nombre: { $first: "$nombre" },
+                    descripcion: { $first: "$descripcion" },
+                    estado: { $first: "$estado" },
+                    created_by: { $first: "$created_by" },
+                    created_at: { $first: "$created_at" },
+                    updated_at: { $first: "$updated_at" },
+                    Total: { $sum: "$Total.cantidad" }
+                }
+            },
+            {
+                $sort: {
+                    "Total": -1
+                }
+            }
+        ];
+        let result = await colleccion.aggregate(query).sort({nombre: 1}).toArray();
+        res.send(result);
     } catch (error) {
         res.status(404).send({ status:404, message: "Query Not Found :(" })
     }
--- routers/productos.js
+++ routers/productos.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { limitQuery } from "../limit/config.js";
-import { getProductos } from "../queries/productos.js";
+import { getProductos , getTotal} from "../queries/productos.js";
 import { appValidateData, appMiddlewareBodega, appDTOData } from "../middleware/productos.js";
 
 const apProductos = express();
 apProductos.use(express.json());
 
 apProductos.get("/all", limitQuery(), appMiddlewareBodega, getProductos);
+apProductos.get("/total", limitQuery(), appMiddlewareBodega, getTotal)
 
 export default apProductos;
