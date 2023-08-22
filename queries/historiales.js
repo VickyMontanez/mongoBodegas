3f8e404d59217cb22af1cf1b77cb1bdf38a09e48
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-22 18:22:11 -0500

    feat: :sparkles: Sixth Query Completed but not tested
    Sixth Query Completed but not tested

--- queries/historiales.js
+++ queries/historiales.js
@@ -1,4 +1,4 @@
-import {conx} from "../db_mg/atlas.js";
+import { conx } from "../db_mg/atlas.js";
 import { ObjectId } from "mongodb";
 
 
@@ -6,6 +6,7 @@ import { ObjectId } from "mongodb";
   "id":31,
   "id_producto":1,
   "cantidad":3,
+  "id_inventario":2,
   "id_bodega_origen":1,
   "id_bodega_destino":2,
   "created_by":1,
@@ -13,8 +14,9 @@ import { ObjectId } from "mongodb";
   "created_at": "2023-08-22",
   "updated_at": "2023-08-22"
 } */
-export async function apptraslado(req, res){
+export async function apptraslado(req, res) {
     try {
+
         const { id_producto, id_bodega_origen, id_bodega_destino, cantidad } = req.body;
         console.log(req.body);
         let db = await conx();
@@ -26,55 +28,47 @@ export async function apptraslado(req, res){
             id_bodega: id_bodega_origen,
             cantidad: { $gte: cantidad }
         });
-        await inventariosColleccion.updateOne(
-            { _id: origenBodega._id },
-            { $inc: { cantidad: -cantidad } }
-        );
 
-        const destinoBodega = await inventariosColleccion.findOne({
-            id_producto: id_producto,
-            id_bodega: id_bodega_destino
-        });
-        
-        if (!destinoBodega) {
-            await historialesColleccion.insertOne(nuevoHistorial)
+        if (!origenBodega || cantidad > origenBodega.cantidad) {
+            res.status(400).send({ status: 400, message: "Bodega Origen insuficiente" });
             return;
+        } else {
+            const resultOrigen = await inventariosColleccion.updateOne(
+                { _id: origenBodega._id },
+                { $inc: { cantidad: -cantidad } }
+            );
+
+            const destinoBodega = await inventariosColleccion.findOne({
+                id_producto: id_producto,
+                id_bodega: id_bodega_destino
+            });
+
+            if (!destinoBodega) {
+                const nuevoInventario = {
+                    ...req.body,
+                    id_bodega: id_bodega_destino,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                };
+
+                await inventariosColleccion.insertOne(nuevoInventario);
+                res.send({ message: "Traslado exitoso, nueva entrada de historial creada", resultOrigen });
+            } else {
+                const resultDestino = await inventariosColleccion.updateOne(
+                    { _id: destinoBodega._id },
+                    { $inc: { cantidad: +cantidad } }
+                );
+
+                const nuevoHistorial = {
+                    ...req.body,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                };
+
+                await historialesColleccion.insertOne(nuevoHistorial);
+                res.send({ message: "Traslado exitoso, entrada de historial creada para destino y origen", resultOrigen, resultDestino });
+            }
         }
-        
-        res.send(destinoBodega)
-        /* const destinoBodega = await inventariosColleccion.findOne({
-            id_producto: id_producto,
-            id_bodega: id_bodega_destino,
-            cantidad: parseInt(cantidad)
-        });
-
-        if (!origenBodega) {
-            res.status(400).send({ status: 400, message: "Invalid traslado request" });
-            return;
-        }
-
-        const nuevaCantidadOrigen = origenBodega.cantidad - cantidad;
-        const nuevaCantidadDestino = destinoBodega.cantidad + cantidad;
-
-        await inventariosColleccion.updateOne(
-            { id: origenBodega.id_bodega_origen },
-            { $set: { cantidad: nuevaCantidadOrigen } }
-        );
-
-        await inventariosColleccion.updateOne(
-            { id: destinoBodega.id_bodega_destino },
-            { $set: { cantidad: nuevaCantidadDestino } }
-        );
-
-        const nuevoHistorial = {
-            ...req.body,
-            created_at: new Date(req.body.created_at),
-            updated_at: new Date(req.body.updated_at)
-        };
-
-        await historialesColleccion.insertOne(nuevoHistorial);
-
-        res.send({ status: 200, message: "Producto trasladado exitosamente" }); */
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ status: 500, message: "Internal Server Error" });
