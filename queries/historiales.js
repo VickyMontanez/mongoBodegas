bd9b5c574f324458c889fbcf941e522a2cda53a3
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-23 09:30:56 -0500

    fix: :pencil2: Fix typos
    Fix typos

--- queries/historiales.js
+++ queries/historiales.js
@@ -1,4 +1,3 @@
-import { error } from "console";
 import { conx } from "../db_mg/atlas.js";
 import { ObjectId } from "mongodb";
 
@@ -6,7 +5,6 @@ export async function apptraslado(req, res) {
     try {
 
         const { id, id_producto, id_bodega_origen, id_bodega_destino, cantidad, created_by, updated_by } = req.body;
-        console.log(req.body);
         let db = await conx();
         let inventariosColleccion = db.collection("inventarios");
         let historialesColleccion = db.collection("historiales");
@@ -14,7 +12,7 @@ export async function apptraslado(req, res) {
         const existingRecord = await historialesColleccion.findOne({ id });
 
         if (existingRecord) {
-            res.status(400).send({ status: 400, message: "El ID ya existe en la base de datos" });
+            res.status(400).send({ status: 400, message: "The ID already exists in the database" });
             return;
         }
 
@@ -25,7 +23,7 @@ export async function apptraslado(req, res) {
         });
 
         if (!origenBodega || cantidad >= origenBodega.cantidad) {
-            res.status(400).send({ status: 400, message: "Bodega Origen insuficiente", });
+            res.status(400).send({ status: 400, message: "Insufficient product in the 'BodegaOrigen'", });
             return;
         } else {
             await inventariosColleccion.updateOne(
@@ -40,28 +38,30 @@ export async function apptraslado(req, res) {
 
             !destinoBodega ? (
                 await inventariosColleccion.insertOne({
-                  id,
-                  id_bodega: id_bodega_destino,
-                  id_producto,
-                  cantidad,
-                  created_by,
-                  updated_by,
-                  created_at: new Date(),
-                  updated_at: new Date()
+                    _id: new ObjectId(),
+                    id,
+                    id_bodega: id_bodega_destino,
+                    id_producto,
+                    cantidad,
+                    created_by,
+                    updated_by,
+                    created_at: new Date(),
+                    updated_at: new Date()
                 }))
             : await inventariosColleccion.updateOne({ _id: destinoBodega._id },{ $inc: { cantidad: +cantidad } });
 
             const nuevoHistorial = {
+                _id: new ObjectId(),
                 ...req.body,
                 created_at: new Date(),
                 updated_at: new Date()
             };
 
             await historialesColleccion.insertOne(nuevoHistorial);
-            res.send({ message: "Traslado exitoso, entrada de historial creada para destino y origen" });
+            res.status(200).send({status: 200, message: "Transfer successful, history entry created successfully" });
         }
     } catch (error) {
-        res.status(500).send({ status: 500, message: "El id proporsionado ya esa en la base de datos" });
-
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+        console.log(error.message);
     }
 };
